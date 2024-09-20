fa8558cf35e26cb9707a1f28221b58e78a602b5b
Robby Williams <liberator8461071@gmail.com>
2024-09-20 15:52:26 -0500

    Second Half of Day BluetoothManager created, and bluetooth functionality migrated

--- app/src/main/java/com/example/rdsmartclipper/BluetoothManager.java
+++ app/src/main/java/com/example/rdsmartclipper/BluetoothManager.java
@@ -46,13 +46,6 @@ public class BluetoothManager {
     public void connectToDevice(String macAddress) {
         if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_CONNECT)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         if(macAddress == null ||macAddress.isEmpty()){
